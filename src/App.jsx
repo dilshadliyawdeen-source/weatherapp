1f3c78a5ac28c938c9228dc1612a9463ebccf9d9
Dilshad Liyawdeen <dilshadliyawdeen@gmail.com>
2025-11-20 17:40:23 +0530

    add App component

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import WeatherApp from "./WeatherApp";
+
+export default function App() {
+  return (
+    <div style={{ minHeight: "100vh", padding: 24, background: "#f1f5f9" }}>
+      <WeatherApp />
+    </div>
+  );
+}
